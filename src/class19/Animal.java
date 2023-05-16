66d067e177897f62caee2f29d929f4656a3fc756
eksdeemephisto <shuyinvenus@gmail.com>
2023-05-16 18:12:19 -0400

    update 5/16/2023 1812

--- src/class19/Animal.java
+++ src/class19/Animal.java
@@ -1,61 +1,69 @@
 package class19;
 
-// create 3 classes like Dog Cat Horse define 5 properties in each class
-// access level
-// create one printInfo method that will print the values of these variables.
+// Create 3 classes like Dog Cat Horse define 5 properties(use default
+// access level).
+// You can use inheritance to avoid code duplication.
+// Create one printInfo method that will print the values of these variables.
 // create constructors as well in each child class to initialize those
 // properties create one object of each class and call the printInfo method
-
 public class Animal {
-    String name;
-    double weight;
-    int age;
-    char gender;
-    String color;
+    private String name;
+    private String color;
+    private String breed;
+    private int age;
+    private double weight;
 
-    public Animal(String name, double weight, int age, char gender, String color) {
+    public Animal(String name, String color, String breed, int age, double weight) {
+
         this.name = name;
-        this.weight = weight;
-        this.age = age;
-        this.gender = gender;
         this.color = color;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
     }
 
     void printInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Weight: " + weight);
-        System.out.println("Age: " + age);
-        System.out.println("Gender: " + gender);
-        System.out.println("Color: " + color);
-        System.out.println();
+        System.out.println(name + " " + color + " " + breed + " " + age + " " + weight);
     }
 
-    public static void main(String[] args) {
-        Dog dog = new Dog("James", 24.2, 7, 'F', "White");
-        dog.printInfo();
 
-        Cat cat = new Cat("Blacky", 12.2, 3, 'M', "Black");
-        cat.printInfo();
-
-        Horse horse = new Horse("Snow", 50, 12, 'M', "White");
-        horse.printInfo();
-    }
 }
 
 class Dog extends Animal {
-    public Dog(String name, double weight, int age, char gender, String color) {
-        super(name, weight, age, gender, color);
+    double price;
+
+    // break till 8:56
+    public Dog(String name, String color, String breed, int age, double weight, double price) {
+        super(name, color, breed, age, weight);
+        this.price = price; // assigning the price
+    }
+    void printInfo(){
+        super.printInfo(); // reusing the printInfo method from parent class by using super keyword
+        System.out.println(price);
     }
 }
 
 class Cat extends Animal {
-    public Cat(String name, double weight, int age, char gender, String color) {
-        super(name, weight, age, gender, color);
+
+    public Cat(String name, String color, String breed, int age, double weight) {
+        super(name, color, breed, age, weight);
     }
 }
 
 class Horse extends Animal {
-    public Horse(String name, double weight, int age, char gender, String color) {
-        super(name, weight, age, gender, color);
+
+    public Horse(String name, String color, String breed, int age, double weight) {
+        super(name, color, breed, age, weight);
+    }
+}
+
+class AnimalTester{
+
+
+
+    public static void main(String[] args) {
+        Dog dog=new Dog("Tom","Black","persian",10,20,200);
+        dog.printInfo();
+
     }
 }
